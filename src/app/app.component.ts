d49f7f2b347982e7fba34a1c9f59e4daa98aadc8
Juan Almanza <115324283+jalmanzaa@users.noreply.github.com>
2023-03-15 22:04:33 -0500

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,33 @@ export class AppComponent {
   title = '2Dimagen';
 
 }
+export function estimate_area(image: any): void {
+  const reader = new FileReader();
+  reader.onload = (event: any) => {
+    const img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        if (imageData) {
+          const data = imageData.data;
+          let area = 0;
+          for (let i = 0; i < data.length; i += 4) {
+            if (data[i] > 0 && data[i + 1] === 0 && data[i + 2] === 0 && data[i + 3] > 0) {
+              area++;
+            }
+          }
+          console.log(`El área estimada de la mancha es de ${area} píxeles`);
+        }
+      }
+    };
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(image);
+}
 
 
--- src/app/image-upload/image-upload.component.ts
+++ src/app/image-upload/image-upload.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { estimate_area } from '../app.component';
 
 @Component({
   selector: 'app-image-upload',
@@ -16,35 +16,6 @@ export class ImageUploadComponent {
   onSubmit() {
     estimate_area(this.image);
   }
-}
-
-export function estimate_area(image: any): void {
-  const reader = new FileReader();
-  reader.onload = (event: any) => {
-    const img = new Image();
-    img.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        ctx.drawImage(img, 0, 0);
-        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        if (imageData) {
-          const data = imageData.data;
-          let area = 0;
-          for (let i = 0; i < data.length; i += 4) {
-            if (data[i] > 0 && data[i + 1] === 0 && data[i + 2] === 0 && data[i + 3] > 0) {
-              area++;
-            }
-          }
-          console.log(`El área estimada de la mancha es de ${area} píxeles`);
-        }
-      }
-    };
-    img.src = event.target.result;
-  };
-  reader.readAsDataURL(image);
 }
 
 
