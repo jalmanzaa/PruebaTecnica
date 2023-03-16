c1a11da720137a62c3e0550b888efbf15825d75b
Juan Almanza <115324283+jalmanzaa@users.noreply.github.com>
2023-03-15 21:23:52 -0500

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageUploadComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/image-upload/image-upload.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-image-upload',
+  templateUrl: './image-upload.component.html',
+  styleUrls: ['./image-upload.component.scss']
+})
+export class ImageUploadComponent {
+  image: any;
+
+  onFileSelected(event: any) {
+    this.image = event.target.files[0];
+  }
+
+  onSubmit() {
+    estimate_area(this.image);
+  }
+}
+
+export function estimate_area(image: any): void {
+  const reader = new FileReader();
+  reader.onload = (event: any) => {
+    const img = new Image();
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext('2d');
+      if (ctx) {
+        ctx.drawImage(img, 0, 0);
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        if (imageData) {
+          const data = imageData.data;
+          let area = 0;
+          for (let i = 0; i < data.length; i += 4) {
+            if (data[i] > 0 && data[i + 1] === 0 && data[i + 2] === 0 && data[i + 3] > 0) {
+              area++;
+            }
+          }
+          console.log(`El área estimada de la mancha es de ${area} píxeles`);
+        }
+      }
+    };
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(image);
+}
+
+
